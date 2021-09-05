cb74fecb42957a9bc46b52e54017454db508f9f9
give your user name here <your.email.id>
2021-09-05 23:43:29 +0300

    new commit

---
+++ Core/Controller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core;
+
+use Exception;
+
+class Controller{
+
+
+    protected $routeParams = [];
+
+    public function __construct($routeParams)
+    {
+        $this->routeParams = $routeParams;
+    }
+ 
+    public function __call(string $method,array $arguments)
+    {
+        if (method_exists($this, $method)) {
+            if ($this->before() !== false) {
+                call_user_func(([$this, $method], $arguments));
+                $this->after();
+            }
+        } else {
+            throw new Exception("Method {$method} not found in controller" . get_class($this));
+        }
+    }
+
+    public function before(): bool
+    {
+        return true;
+    }
+
+    public function after(): void {}
+}
